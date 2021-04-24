d0e81eba4b2f7df9c6709d622f25ef2d6950e0ad
Michał Kleszczyński <michalkamilkleszczynski@gmail.com>
2021-04-24 18:52:34 +0200

    Sensitive data searcher

---
+++ src/sensitive-data-searcher.js
@@ -0,0 +1,55 @@
+const sensitiveKeywords = require('./sensitive-keywords')
+
+class SensitiveDataSearcher {
+    constructor () {
+        this.regexes = []
+    }
+
+    build () {
+        const expectedCharacters = `\w !@#$%^&()\\\/,.;'-_`
+
+        this.regexes = sensitiveKeywords.map(keyword => {
+            return [
+                // Most C-Style languages
+                new RegExp(`${keyword} ?= ?["'\`]?([${expectedCharacters}]+)["'\`]?;?`),
+
+                // Type of variable after comma - Kotlin/Typescript
+                new RegExp(`${keyword} ?: ?[a-zA-Z0-9_]+ ?= ?["'\`]?([${expectedCharacters}]+)["'\`]?`)
+            ];
+        }).flat()
+    }
+
+    /**
+     *
+     * @param {string} file
+     * @param {string} contents
+     */
+    search (file, contents) {
+        const problems = []
+
+        this.regexes.forEach(regex => {
+            contents.split('\n').forEach((line, idx) => {
+                line = line.trim()
+                const match = line.match(regex)
+
+                if (match) {
+                    problems.push({
+                        file: file,
+                        line: {
+                            number: idx,
+                            contents: line
+                        },
+                        type: 'Sensitive data'
+                    })
+                }
+            })
+        })
+
+        return {
+            problems: problems,
+            valid: problems.length === 0
+        }
+    }
+}
+
+module.exports = SensitiveDataSearcher
