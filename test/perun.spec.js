2eb72eb087b8f2a3912eb2f77e81d7e1d2f92674
Michał Kleszczyński <michalkamilkleszczynski@gmail.com>
2021-05-22 10:14:05 +0200

    Unit tests schema

--- test/perun.spec.js
+++ test/perun.spec.js
@@ -1,10 +1,63 @@
+const accessSecretVersion = require('../src/secret-gcloud')
+const os = require('os')
 const Perun = require('../src/perun')
+const SensitiveDataSearcher = require('../src/sensitive-data-searcher')
+
+jest.mock('../src/sensitive-data-searcher')
+jest.mock('../src/sensitive-data-searcher')
 
 describe('perun', () => {
     it('instantiates', () => {
         expect(() => {
-            // eslint-disable-next-line no-new
-            new Perun()
+            const perun = new Perun()
+
+            expect(perun).toBeTruthy()
         }).not.toThrow()
+    })
+
+    it('should set default parameters', () => {
+        const perun = new Perun()
+
+        expect(perun.debug).toBe(true)
+        expect(perun.cloneDir.startsWith(os.tmpdir())).toBe(true)
+        expect(perun.foundProblems).toBeInstanceOf(Array)
+        expect(perun.foundProblems).toHaveLength(0)
+        expect(perun.searcher).toBeInstanceOf(SensitiveDataSearcher)
+    })
+
+    describe('run', () => {
+
+    })
+
+    describe('cloneRepository', () => {
+
+    })
+
+    describe('process', () => {
+
+    })
+
+    describe('analyzeFile', () => {
+
+    })
+
+    describe('lookForSensitiveData', () => {
+
+    })
+
+    describe('lookForSqlInjection', () => {
+
+    })
+
+    describe('cleanup', () => {
+
+    })
+
+    describe('logRaw', () => {
+
+    })
+
+    describe('log', () => {
+        
     })
 })
